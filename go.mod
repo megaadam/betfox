89bcc43ae7293136956254c65139c53cc92b9f07
Adamski <megaadam@yahoo.com>
2021-09-01 22:15:42 +0200

    almost logging in

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/megaadam/betfox
 
 go 1.16
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-openapi/analysis v0.20.1 // indirect
+	github.com/go-openapi/errors v0.20.1
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/runtime v0.19.31
+	github.com/go-openapi/strfmt v0.20.2
+	github.com/go-openapi/swag v0.19.15
+	github.com/go-openapi/validate v0.20.2
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- session/session.go
+++ session/session.go
@@ -2,18 +2,70 @@ package session
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/megaadam/betfox/betfair/client"
+	"github.com/megaadam/betfox/betfair/client/operations"
+	"github.com/megaadam/betfox/betfair/models"
 )
 
 type creds struct {
-	User string `json:"user"`
-	Pass string `json:"pass"`
+	User   string `json:"user"`
+	Pass   string `json:"pass"`
+	AppKey string `json:"appKey"`
 }
 
-// Fundamental Login
+// Login --stuff
 func Login() {
 	file, _ := ioutil.ReadFile("/home/a/.betfair/creds.json")
 	creds := creds{}
 	_ = json.Unmarshal([]byte(file), &creds)
 
+	reqURL, _ := url.Parse("https://identitysso-cert.betfair.se:443/api/certlogin")
+
+	// create request body
+	reqBody := ioutil.NopCloser(strings.NewReader("username=" + creds.User + "&password=" + creds.Pass))
+	// 	{
+	// 		"username":creds.user,
+	// 		"pasword":creds.password,
+	// 	}
+	// `))
+
+	// create a request object
+	req := &http.Request{
+		Method: "POST",
+		URL:    reqURL,
+		Header: map[string][]string{
+			"Content-Type":  {"application/x-www-form-urlencoded"},
+			"X-Application": {"jvisaiEIJz06FUlF"},
+		},
+		Body: reqBody,
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	fmt.Println(res, err)
+	// read response body
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Printf("%s\n", data)
+
+	cm := models.ConnectionMessage{}
+	cm.ConnectionID = ""
+
+	params := operations.PostRequestParams{}
+
+	auth := new(models.AuthenticationMessage)
+	auth.AppKey = creds.AppKey
+	auth.Session = "SESSIONTOKEN"
+
+	params.RequestMessage = new(models.AllRequestTypesExample)
+	params.RequestMessage.Authentication = auth
+	params.RequestMessage.Heartbeat = &models.HeartbeatMessage{}
+
+	cli := client.Default.Operations.PostRequest(&params)
+	fmt.Println(cli)
 }
